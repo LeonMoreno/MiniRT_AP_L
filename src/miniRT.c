384703e76b7cbdb200da9a1cac9ba256d38dfcb3
Leonardo Moreno <lmoreno@student.42quebec.com>
2022-08-22 17:39:06 -0400

    malloc t_mini

--- src/miniRT.c
+++ src/miniRT.c
@@ -2,12 +2,13 @@
 
 int	main(int argc, char **argv)
 {
-	t_mini	m;
+	t_mini	*m;
 
-	start_scene(argc, argv, &m);
-	mlx_start(&m);
-	//start_render(&m);
-	mlx_put_image_to_window(m.mlx, m.win, m.i.img, 0, 0);
-	mlx_loop(m.mlx);
+	m = malloc(sizeof(t_mini));
+	start_scene(argc, argv, m);
+	mlx_start(m);
+	//start_render(m);
+	mlx_put_image_to_window(m->mlx, m->win, m->i.img, 0, 0);
+	mlx_loop(m->mlx);
 	return (0);
 }
