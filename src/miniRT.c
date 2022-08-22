eda9ed52b18621913a8b1f573aed1d5fa33a7e9d
Leonardo Moreno <lmoreno@student.42quebec.com>
2022-08-22 17:13:30 -0400

    clean for begin with AP

--- src/miniRT.c
+++ src/miniRT.c
@@ -5,9 +5,9 @@ int	main(int argc, char **argv)
 	t_mini	m;
 
 	start_scene(argc, argv, &m);
-	//mlx_start(&m);
+	mlx_start(&m);
 	//start_render(&m);
-	//mlx_put_image_to_window(m.mlx, m.win, m.i.img, 0, 0);
-	//mlx_loop(m.mlx);
+	mlx_put_image_to_window(m.mlx, m.win, m.i.img, 0, 0);
+	mlx_loop(m.mlx);
 	return (0);
 }
