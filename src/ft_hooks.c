c4219987c2b283d45d3fa6b2fd61cd4dcc4dac2e
Andre-paul Grenon <grenonap@gmail.com>
2022-09-14 16:24:19 -0400

    norminette

--- src/ft_hooks.c
+++ src/ft_hooks.c
@@ -6,7 +6,7 @@
 /*   By: lmoreno <lmoreno@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:31:36 by lmoreno           #+#    #+#             */
-/*   Updated: 2022/09/14 16:03:31 by lmoreno          ###   ########.fr       */
+/*   Updated: 2022/09/14 16:23:48 by agrenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	ft_key_hook(int keyhook, t_mini *m)
 	event_pl(m, keyhook);
 	event_ca(m, keyhook);
 	event_li(m, keyhook);
- 	printf("key = %d\n", keyhook);
+	printf("key = %d\n", keyhook);
 	return (0);
 }
 
